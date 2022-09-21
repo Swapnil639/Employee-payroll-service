9a3ee0f4f93407da155e7f46d99d314f81ecb900
Swapnil639 <swapnilpatil631997@gmail.com>
2022-09-21 20:03:37 +0530

    Added :Stored employee payroll into a file

---
+++ src/test/java/com/bridgelabz/EmployeePayrollServiceTest.java
@@ -0,0 +1,23 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class EmployeePayrollServiceTest {
+
+    @Test
+    public void given3EmployeeWhenWrittenToFileShouldMatchEmployeeEntries() {
+        EmployeePayrollData[] arrayOfEmp = {
+                new EmployeePayrollData(1, "Swapnil", 20000.0),
+                new EmployeePayrollData(2, "Shubham", 40000.0),
+                new EmployeePayrollData(3, "Sahil", 50000.0)
+        };
+        EmployeePayrollService employeePayRollService;
+        employeePayRollService = new EmployeePayrollService(Arrays.asList(arrayOfEmp));
+        employeePayRollService.writeEmployeePayrollData(EmployeePayrollService.IOService.FILE_IO);
+        long entries = employeePayRollService.countEntries(EmployeePayrollService.IOService.FILE_IO);
+        Assert.assertEquals(3, entries);
+    }
+}
